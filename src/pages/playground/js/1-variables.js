c70cb8a97655a57f7bc06f992031104135ab4422
loic berthollet <loic.berthollet@hotmail.com>
2023-10-24 23:53:46 +0200

    improve comments

--- src/pages/playground/js/1-variables.js
+++ src/pages/playground/js/1-variables.js
@@ -13,84 +13,90 @@ Note: On peut débuger avec VScode; on ne peut faire que des console.log avec no
 // eslint-disable-next-line no-unused-vars
 // import * as util from "utils";
 
-console.log("----------------------------------------");
+console.log("---------------------------------------- JS");
 console.log("But: Découvrir les variables");
-console.log("----------------------------------------");
+console.log("---------------------------------------- JS");
 
 /* ==================================================================================================================== 
 Une variable:
     - contient une ou plusieurs valeurs.
+    - n'existe uniquement dans la portée/le bloc ou elle est définie. 
     - est d'un type: (JavaScript = language à typage FAIBLE ==> le type de la variable peut changer quand on la réassigne)
-        - chaine de caractère: "", "a", "arnaud", "quelle belle journée"
-        - booléen : true, false
-        - nombre : entier ou décimal (à virgule), 0, -50, 350.123456
-        - date ou heure, ou les deux
-        - un tableau: ["marc", 22, 12:19]
-        - un objet : {name:"peter", birthday:2001} --------> Ici, cet objet  définit une personne: 2 propriétés (marc et birthday) ainsi que leurs valeurs
+        - Type "chaine de caractère" (string) : "", "a", "arnaud", "quelle belle journée"
+        - Type "booléen" (bool) : true, false
+        - Type "nombre" (number) : entier ou décimal (à virgule), 0, -50, 350.123456
+        - Type "date" ou "heure", ou les "deux" (Date)
+        - Type "tableau"  (Array)) : ["marc", 22, 12:19]
+        - Type "objet" (object) : {name:"peter", birthday:2001} ---> Ici, cet objet définit une personne: 2 propriétés (marc et birthday) et leurs valeurs
     - doit être DÉCLARÉE et INITIALISÉE (en une fois ou en deux fois)
-        - DÉCLARÉE: - déclare à node qu'on veut utiliser une variable dont on donne le nom. 
+        - DÉCLARÉE: - déclare à node qu'on veut utiliser une variable dont on donne le nom.
+                      Ex.: let name;
                     - Node crée alors un emplacement mémoire pour contenir cette variable.
-                    - Cette variable ne contient alors aucune valeur, on dit qu'elle est indéfinie
-        - INITIALISÉE: donne une valeur à la variable
-
-    - existe uniquement dans la portée/bloc ou ellee est définie. 
+                    - Cette variable ne contient alors aucune valeur, elle est indéfinie (undefined)
+        - INITIALISÉE ou ASSIGNÉE: donne une valeur à la variable
 ==================================================================================================================== */
 
 // --------------------------------------------------------------------------------------------------------------------
 // On DÉCLARE une variable SANS l'initialiser :  Ici on déclare une variable qui s'appelle firstname;
+// note: déclarée mais sont assignée: sa valeur est 'undefined'.
 let firstname;
-console.log("la valeur de la variable 'firstname' = " + firstname); // écrit dans la console la VALEUR de firstname;
+console.log("la valeur de la variable 'firstname' est égal à " + firstname); // écrit dans la console la VALEUR de firstname (cad 'undefined)
 
-// On INITIALISE la valeur de la variable 'firstname'
+// On INITIALISE/ASSIGNE la valeur de la variable 'firstname'
 firstname = "Arnaud";
-console.log("la valeur de la variable 'firstname' = " + firstname); // écrit dans la console la VALEUR de firstname;
+console.log("valeur variable 'firstname' = " + firstname); // écrit dans la console la VALEUR de firstname;
 
-// On MODIFIE la valeur de la variable 'firstname'
-firstname = "Loic";
-console.log("la valeur de la variable 'firstname' = " + firstname); // écrit dans la console la VALEUR de firstname;
+// On MODIFIE/REASSIGNE la valeur de la variable 'firstname'
+firstname = "Thomas";
+console.log("valeur variable 'firstname' = " + firstname); // écrit dans la console la VALEUR de firstname;
 // --------------------------------------------------------------------------------------------------------------------
 
 // --------------------------------------------------------------------------------------------------------------------
-// On DÉCLARE une variable ET on INITIALISE :  Ici on déclare une variable qui s'appelle age et qui contient la valeur 25
+// On DÉCLARE une variable ET on INITIALISE en même temps : la variable s'appelle "age" et contient la valeur 25
 let age = 25;
-console.log("la valeur de la variable 'age' = " + age);
+console.log("valeur variable 'age' = " + age);
 
-// On MODIFIE la valeur de la variable "age". Ici on lui ajoute 10
+// On MODIFIE/REASSIGNE la valeur de la variable "age". Ici on lui ajoute 10
 age = age + 10;
-console.log("la valeur de la variable 'age' = " + age);
+console.log("valeur variable 'age' = " + age);
 
 // On DÉCLARE une variable ET on l'INITIALISE avec une partie fixe ET la valeur de la variable "firstname"
-// le mot-clé "cont" signifie que la valeur de la variable ne doit pas changer.
+// le mot-clé "const" signifie que la valeur de la variable ne doit pas changer.
 const welcome = "Hello " + firstname;
-console.log("la valeur de la variable 'welcome' = " + welcome);
-// Si on dé-commente la ligne suivante, cela va provoquer une erreur d'exécution (car on tente de modifier une constante)
+console.log("valeur variable 'welcome' = " + welcome);
+
+// Si on dé-commente la ligne suivante, cela va provoquer une erreur d'exécution car on tente de modifier une constante
 // welcome = "toto";
 // --------------------------------------------------------------------------------------------------------------------
 
 // gender = 12;   ---------> Ici, erreur car la variable est initialisée sans avoir été déclarée préalablement.
 
+console.log('\nAffichage type variable x, et valeur variable x:')
+
 // --------------------------------------------------------------------------------------------------------------------
-let x = true; // La variable "x" est de type booléen, sa valeur est vraie.
+let x = true; // La variable "x" est de type booléen, sa valeur est 'vraie'.
 console.log(typeof x, x); // affiche dans la console le type de la variable "x", puis la valeur de "x"
 
-// réassigne la valeur de la variable x = date de maintenant
+// réassigne la valeur de la variable x en changeant aussi son type: valeur= date de maintenant; type= Date
 x = new Date();
 console.log(typeof x, x);
 
-// réassigne la valeur de la variable x = surface d'un cercle de rayon 4cm (s = PI x R²)
+// réassigne la valeur de la variable x en changeant aussi son type: valeur= surface cercle rayon 4cm (s = PI x R²), type= number
 x = Math.PI * 4 * 4;
 console.log(typeof x, x);
 
-// réassigne la valeur de la variable x = un tableau avec 7 éléments de types différents (le dernier est lui-même un tableau)
+// réassigne la valeur de la variable x en changeant aussi son type: valeur= tableau de 7 éléments de types différents (le dernier est lui-même un tableau)
 x = ["arnaud", "thomas", "loïc", "agnès", 12, false, [1, 2, 3]];
 console.log(typeof x, x);
 
-// réassigne la valeur de la variable x = un objet avec des propriétés (firstname, birthday...)
-x = { firstname: "toto", weight: 70, birthday: new Date(1970, 12, 31) }; // Ce firsname n'est pas celui défini plus haut, car ici, il est dans le bloc de code de "x"
+// réassigne la valeur de la variable x en changeant aussi son type: valeur= un objet avec des propriétés "firstname" et "birthday"
+x = { firstname: "Arnaud", weight: 55, birthday: new Date(1995, 8, 25) }; // Ce firstname n'est pas celui défini plus haut, car ici, il est dans le bloc de code de "x"
 console.log(typeof x, x);
 
-// calcul de l'age de toto
+// calcul de l'age de toto (réutilise la variable 'toto' définie plus haut dans cette page)
 age = new Date().getFullYear() - x.birthday.getFullYear();
 // affiche une phrase qui donne le poids et l'age de toto.
-console.log(x.firstname + " pèse " + x.weight + " kg. Il a " + age + " ans.");
+console.log(x.firstname + " pèse " + x.weight + " kg, il a " + age + " ans.");
+// même phrase, mais avec une technique différente (string interpolation)
+console.log(`${x.firstname} pèse ${x.weight} kg, il a ${age} ans.`);
 // --------------------------------------------------------------------------------------------------------------------
