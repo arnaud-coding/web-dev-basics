e3f06f9300560b6025307ce133b2222e16ef8e67
loic-b <loic.berthollet@hotmail.com>
2023-10-29 21:08:05 +0100

    format settings

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,25 +1,32 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true
-  },
-  extends: ["standard", "prettier"],
-  overrides: [
-    {
-      env: {
-        node: true
-      },
-      files: [".eslintrc.{js,cjs}"],
-      parserOptions: {
-        sourceType: "script"
-      }
+    env: {
+        browser: true,
+        es2021: true
+    },
+    extends: ["standard", "prettier"],
+    overrides: [
+        {
+            env: {
+                node: true
+            },
+            files: [".eslintrc.{js,cjs}"],
+            parserOptions: {
+                sourceType: "script"
+            }
+        }
+    ],
+    parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module"
+    },
+    rules: {
+        "max-len": ["warn", {
+            code: 120,
+            comments: 160,
+            ignoreStrings: true,
+            ignoreTrailingComments: true,
+            ignoreUrls: true,
+            ignoreTemplateLiterals: true
+        }]
     }
-  ],
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module"
-  },
-  rules: {
-    "max-len": ["warn", { code: 120, comments: 160 }]
-  }
 };
