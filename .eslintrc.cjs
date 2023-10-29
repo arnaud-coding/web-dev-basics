dcd7f9293af811d44c4094546ce067c2c9855b41
arnaud-coding <arnaudberthollet@hotmail.fr>
2023-10-29 18:44:17 +0100

    change line length to 120

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,23 +1,25 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
+    es2021: true
   },
   extends: ["standard", "prettier"],
   overrides: [
     {
       env: {
-        node: true,
+        node: true
       },
       files: [".eslintrc.{js,cjs}"],
       parserOptions: {
-        sourceType: "script",
-      },
-    },
+        sourceType: "script"
+      }
+    }
   ],
   parserOptions: {
     ecmaVersion: "latest",
-    sourceType: "module",
+    sourceType: "module"
   },
-  rules: {},
+  rules: {
+    "max-len": ["warn", { code: 120, comments: 160 }]
+  }
 };
