ad7e477b11d54c17780411a9c717491c154bfd49
loic-b <loic.berthollet@hotmail.com>
2023-12-03 18:49:34 +0100

    commentaires

---
+++ src/pages/playground/js/12-async-await.js
@@ -0,0 +1,20 @@
+/** =================================================================================================================
+   * 
+   * async/await est une facon géniale d'écrire les promesses.
+   * 
+   * ------------------------------------------------------------------------------------------------------------------
+   * 
+   * Un async/await peut réussir ou échouer (comme un promesse).
+   * Un async/await peut utiliser une fonction qui renvoie une promesse.
+   *
+   * --------------------------------------------------------------------------
+   * 
+   * => Toutes les promesses PEUVENT et DOIVENT etre remplacées par des async/await
+   * --------------------------------------------------------------------------
+   * Les promesses servent très majoritairement pour appeler des fonctions asynchrones (ex. : lecture/écriture de fichiers, requêtes API, ...)
+   * Ici, on ne verra que des appels synchrones
+   * =============================================================================================================== */
+
+(() => {
+  // TODO
+})()
