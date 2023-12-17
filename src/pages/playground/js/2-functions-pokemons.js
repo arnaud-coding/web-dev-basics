88dd8b24519d6798d4cb6597533f2155f43932b1
arnaud-coding <arnaudberthollet@hotmail.fr>
2023-12-17 18:07:33 +0100

    pokemons suite

--- src/pages/playground/js/2-functions-pokemons.js
+++ src/pages/playground/js/2-functions-pokemons.js
@@ -1,4 +1,6 @@
 (async () => {
+  // #region demo basique
+
   /**
    * enlève tout les espaces en début et en fin de chaque élément d'un tableau de strings
    * @param {string[]} sentences
@@ -36,6 +38,8 @@
 
   const arr = trimSplit("brian is in the kitchen, lives in london", "in");
   console.log("arr:", arr);
+
+  // #endregion
 
   // ===============================================================
 
@@ -80,40 +84,65 @@
     return [];
   };
 
+  // -------------------------------------------------
+  // Définition d'une classe Inspector
+  // -------------------------------------------------
   /**
-   * cherche tous les id des pokemons qui ont une évolution mega
-   * @param {Pokemon[]} pokemons
-   * @return {number[]}
+   * Inspecte les pokemon
    */
-  const getMegasId = (pokemons) => {
-    const megas = pokemons.filter((poke) => poke.evolution?.mega).map((poke) => poke.pokedexId);
-    console.log(`get ${megas.length} pokemons with a mega evolution`);
-    return megas;
-  };
+  class PokemonInspector {
+    /**
+     * Instancie l'inspecteur avec un tableau de pokemens
+     * @param {Pokemon[]} pokemons
+     */
+    constructor(pokemons) {
+      /** @type {Pokemon[]} */
+      this.pokemons = pokemons; // stocke le tableau de pokemon passé en arguments dans une variable propre à la classe
+    }
 
-  /**
-   * cherche le pokemon dont on donne l'id
-   * @param {Pokemon[]} pokemons
-   * @param {number} id
-   * @returns {Pokemon | undefined}
-   */
-  const getPokemonById = (pokemons, id) => pokemons.find((pokemon) => pokemon.pokedexId === id);
+    /**
+     * cherche tous les id des pokemons qui ont une évolution mega
+     * @return {number[]}
+     */
+    getMegasId = () => {
+      const megas = this.pokemons.filter((poke) => poke.evolution?.mega).map((poke) => poke.pokedexId);
+      console.log(`get ${megas.length} pokemons with a mega evolution`);
+      return megas;
+    };
 
-  // vq chercher tous les pokemons...
+    /**
+     * cherche le pokemon dont on donne l'id
+     * @param {number} id
+     * @returns {Pokemon | undefined}
+     */
+    getPokemonById = (id) => this.pokemons.find((pokemon) => pokemon.pokedexId === id);
+  }
+
+  // -------------------------------------------------
+  // Utilisation de la classe Inspector :
+  //
+  //   NB : Habitulellement la classe est défine dans un autre fichier
+  // -------------------------------------------------
+
+  // va chercher tous les pokemons...
   const pokemons = await fetchPokemons();
   // vérifie si pokemons est un tableau
   if (Array.isArray(pokemons)) {
     // vérifie si le tableau des pokemons contient au moins un élément
     if (pokemons.length) {
-      // chope tous pk qui ont une evo mega
-      const megasId = getMegasId(pokemons);
+      // à partir d'ici, ON EST SUR  d'avoir un tableau de pokemons
+      // ---------------------------------------------------------
+      const inspector = new PokemonInspector(pokemons);
+
+      // chope tous pokemons qui ont une evo mega
+      const megasId = inspector.getMegasId();
       console.log("megasId:", megasId);
 
       // const id = megasId[0];
       // const pokemon = getPokemonById(pokemons, id);
       // console.log("pokemon:", pokemon?.name.fr, pokemon?.evolution?.mega?.[0]);
       for (const id of megasId) {
-        const pokemon = getPokemonById(pokemons, id);
+        const pokemon = inspector.getPokemonById(id);
         console.log("pokemon:", pokemon?.name.fr, pokemon?.evolution?.mega?.[0]);
       }
     }
