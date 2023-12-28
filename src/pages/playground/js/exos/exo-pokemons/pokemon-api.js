e457b3523403e8e5e432d4fcc2c4b206e48cd1c8
arnaud-coding <arnaudberthollet@hotmail.fr>
2023-12-28 16:17:17 +0100

    use session storage

--- src/pages/playground/js/exos/exo-pokemons/pokemon-api.js
+++ src/pages/playground/js/exos/exo-pokemons/pokemon-api.js
@@ -1,14 +1,30 @@
 /**
- * va chercher sur internet tous les pokémons
+ * va chercher sur internet ou dans la session tous les pokémons
  * @returns {Promise<Pokemon[]>} promesse d'un tableau de pokémons
  */
 export const fetchPokemons = async () => {
-  // allez hop, va chercher...
+  // ----- si les pokemons sont deja stockés dans la session, on les utilise
+  try {
+    const pokemonsItems = sessionStorage.getItem("pokemons")
+    if (pokemonsItems !== null) {
+      const pokemons = JSON.parse(pokemonsItems)
+      console.log(`get from session storage ${pokemons.length} pokemons`);
+      return pokemons
+    }
+  } catch (error) {
+    console.log("error while fetching pokemons", error)
+  }
+
+  // ----- les pokemons ne sont pas dans la session, on va les chercher sur internet
   const response = await fetch("https://tyradex.tech/api/v1/pokemon");
   if (response.ok) {
     // transforme la réponse en un object de type JSON
     const pokemons = await response.json();
     console.log(`Fetched ${pokemons.length} pokemons`);
+
+    // stocke les pokemons dans la session courante
+    sessionStorage.setItem("pokemons", JSON.stringify(pokemons))
+
     return pokemons;
   }
   console.error("erreur fetch pokemons", response.status, response.statusText);
