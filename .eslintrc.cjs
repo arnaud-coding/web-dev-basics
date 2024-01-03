7a705ad79478a634d2e12ecafc7b43879eda817f
loic berthollet <loic.berthollet@hotmail.com>
2024-01-03 22:26:26 +0100

    update setup

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,32 +1,32 @@
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true
-    },
-    extends: ["standard", "prettier"],
-    overrides: [
-        {
-            env: {
-                node: true
-            },
-            files: [".eslintrc.{js,cjs}"],
-            parserOptions: {
-                sourceType: "script"
-            }
-        }
-    ],
-    parserOptions: {
-        ecmaVersion: "latest",
-        sourceType: "module"
-    },
-    rules: {
-        "max-len": ["warn", {
-            code: 120,
-            comments: 160,
-            ignoreStrings: true,
-            ignoreTrailingComments: true,
-            ignoreUrls: true,
-            ignoreTemplateLiterals: true
-        }]
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: ["standard", "prettier"],
+  overrides: [
+    {
+      env: {
+        node: true
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script"
+      }
     }
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module"
+  },
+  rules: {
+    "max-len": ["warn", {
+      code: 120,
+      comments: 160,
+      ignoreStrings: true,
+      ignoreTrailingComments: true,
+      ignoreUrls: true,
+      ignoreTemplateLiterals: true
+    }]
+  }
 };
