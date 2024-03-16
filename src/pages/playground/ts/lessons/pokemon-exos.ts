45b983af44b7965a4b8463f63ba6908307022941
Arnaud Berthollet <arnaudberthollet@hotmail.fr>
2024-03-16 18:44:16 +0100

    exo-pokemon --> fonctions usuelles

--- src/pages/playground/ts/lessons/pokemon-exos.ts
+++ src/pages/playground/ts/lessons/pokemon-exos.ts
@@ -43,11 +43,11 @@ import { Pokemon } from './pokemon-types.ts'
   let names = ['Arnaud', 'Loïc', 'Agnès', 'Joëlle']
   console.log(`${names.join(' + ')} are in the house`)
 
-  // slice() renvoie une portion du tableau (début et fin optionnels en arguments)
+  // slice() retourne une portion du tableau SANS l'enlever du tableau original (début et fin optionnels en arguments)
   const sliced = pokemons.slice(50, 55)
   console.log('; ~ sliced:', sliced)
 
-  // splice() enlève et retourne une portion du tableau
+  // splice() retourne une portion du tableau et l'ENLEVE du tableau original (début et fin optionnels en arguments)
   let spliced = names.splice(0, 1)
   console.log('; ~ spliced:', spliced)
   console.log(`${names.join(' + ')} are in the house`)
@@ -60,20 +60,81 @@ import { Pokemon } from './pokemon-types.ts'
   //#endregion
 
   // -------------------------------------
-  // map() : crée un tableau depuis un autre tableau, puis le retourne
+  // map() : Crée un nouveu tableau avec les résultats de l'appel d'une fonction fournie sur chaque élément du tableau appelant
   // -------------------------------------
-  // crée un tableau de noms de pokemons
-  names = pokemons.map()
+  const splittedNames = names.map((name) => name.split(''))
+  console.log('; ~ splittedNames:', splittedNames)
+  let x = pokemons.map((pokemon) => pokemon.name.en)
+  x = pokemons.slice(1, 11).map((pokemon) => pokemon.name.jp)
+
+  // resultat attentu: [ 'name (res 1, res n)', ...]
+  x = pokemons.slice(1, 11).map((pokemon) => {
+    const resistances = pokemon.resistances.map((resistance) => resistance.name).join(', ')
+    return `${pokemon.name.fr} (${resistances})`
+  })
+
+  // resultat attentu: [ 'name (n resistances)', ...]
+  x = pokemons.slice(1, 11).map((pokemon) => {
+    return `${pokemon.name.fr} (${pokemon.resistances.length} resistances)`
+  })
+
+  // resultat attentu: [ 'name (type 1, type n)', ...]
+  x = pokemons
+    .slice(1, 11)
+    .map((pokemon) => `${pokemon.name.fr} (${pokemon.types?.map((type) => type.name).join(', ')})`)
+
+  // resultat attentu: [ 'name (previous evos: [evo 1, evo n], next evos: [evo 1, evo n]...)', ...]
+  x = pokemons.slice(1, 11).map((pokemon) => {
+    const prev = pokemon.evolution?.pre?.map((evo) => evo.name).join(', ')
+    const next = pokemon.evolution?.next?.map((evo) => evo.name).join(', ')
+    return `${pokemon.name.fr} (previous: [${prev ?? ''}], next: [${next ?? ''}])`
+  })
 
   // -------------------------------------
-  // filter()
+  // filter() : Crée un nouveau tableau "filtré": contenant tous les éléments du tableau d'origine remplissant une condition déterminée par la fn cb
   // -------------------------------------
+  const longNames = names.filter((name) => name.length >= 5)
+  console.log('; ~ longNames:', longNames)
+
+  x = pokemons.filter((pokemon) => pokemon.generation === 1).map((pokemon) => pokemon.name.fr)
+
+  // tous les pokemons entre 25 et 30 kg
+  x = pokemons
+    .slice(1)
+    .filter((pokemon) => {
+      // extraire poids: '6,9 Kg' => '6.9'
+      const ws = pokemon.weight.split(' ')[0].replace(',', '.')
+      const w = Number(ws)
+      // élimine ceux dont on n'arrive pas à extraire le poids
+      if (Number.isNaN(w)) {
+        return false
+      }
+
+      // filtrer poids < 25 && > 30
+      // const res = w > 25 && w < 30
+      const res = w > 700
+      return res
+    })
+    .map((pokemon) => `${pokemon.name.fr}, gen ${pokemon.generation} , ${pokemon.weight}`)
+  console.log('; ~ x:', x)
+  return
 
   // -------------------------------------
-  // find()
+  // find() : Renvoie la valeur du 1er élément trouvé dans le tableau qui respecte la condition donnée par la fonction de test passée en argument.
+  //          Sinon, la valeur undefined est renvoyée
   // -------------------------------------
+  const notes = [11, 15, 8, 3, 18]
+  const found = notes.find((element) => element > 10)
+  console.log('; ~ found:', found)
 
   // -------------------------------------
-  // reduce()
+  // reduce() : applique une fonction qui "accumule" chaque valeur d'un tableau de gauche à droite afin de le réduire à une seule valeur.
   // -------------------------------------
+  const array = [1, 2, 3, 4]
+
+  // 0 + 1 + 2 + 3 + 4
+  const initialValue = 0
+  const sumWithInitial = array.reduce((accumulator, currentValue) => accumulator + currentValue, initialValue)
+  console.log('; ~ sumWithInitial:', sumWithInitial)
+  // expected output: 10
 })()
