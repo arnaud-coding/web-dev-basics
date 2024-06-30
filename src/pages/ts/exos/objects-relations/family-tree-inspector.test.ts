53e6a9fb46b8bd8d9c6b9e0b5f8de9aa3ae90966
Arnaud Berthollet <arnaudberthollet@hotmail.fr>
2024-06-30 16:35:08 +0200

    family-tree inspector done

--- src/pages/ts/exos/objects-relations/family-tree-inspector.test.ts
+++ src/pages/ts/exos/objects-relations/family-tree-inspector.test.ts
@@ -1,5 +1,5 @@
 import { beforeEach, describe, expect, test } from 'vitest'
-import { FamilyTree, Gender, Person } from './family-tree.model.ts'
+import { FamilyTree, Gender, Person, Relation } from './family-tree.model.ts'
 import { FamilyTreeInspector } from './family-tree-inspector.ts'
 import { FamilyTreeCreator } from './family-tree-creator.ts'
 
@@ -57,43 +57,43 @@ function createMembers() {
 
 function createBertholletTree(): FamilyTree {
   createMembers()
-  const x = new FamilyTreeCreator('Berthollet', arnaud)
-  x.addParent(loic, arnaud)
-  x.addParent(agnes, arnaud)
-  x.addChild(thomas, loic)
-  x.addRelationship(thomas, 'child', agnes)
+  const creator = new FamilyTreeCreator('Berthollet', arnaud)
+  creator.addParent(loic, arnaud)
+  creator.addParent(agnes, arnaud)
+  creator.addChild(thomas, loic)
+  creator.addRelationship(thomas, 'child', agnes)
 
-  x.addParent(jo, loic)
-  x.addParent(annick, loic)
-  x.addChild(philippe, jo)
-  x.addRelationship(philippe, 'child', annick)
+  creator.addParent(jo, loic)
+  creator.addParent(annick, loic)
+  creator.addChild(philippe, jo)
+  creator.addRelationship(philippe, 'child', annick)
 
-  x.addParent(pierrot, agnes)
-  x.addParent(millette, agnes)
-  x.addChild(herve, pierrot)
-  x.addRelationship(herve, 'child', millette)
-  x.addChild(xavier, pierrot)
-  x.addRelationship(xavier, 'child', millette)
+  creator.addParent(pierrot, agnes)
+  creator.addParent(millette, agnes)
+  creator.addChild(herve, pierrot)
+  creator.addRelationship(herve, 'child', millette)
+  creator.addChild(xavier, pierrot)
+  creator.addRelationship(xavier, 'child', millette)
 
-  x.addChild(romain, philippe)
-  x.addRelationship(romain, 'child', nathalie)
-  x.addChild(anthony, philippe)
-  x.addRelationship(anthony, 'child', nathalie)
+  creator.addChild(romain, philippe)
+  creator.addRelationship(romain, 'child', nathalie)
+  creator.addChild(anthony, philippe)
+  creator.addRelationship(anthony, 'child', nathalie)
 
-  x.addChild(clement, herve)
-  x.addRelationship(clement, 'child', agnesMeunier)
-  x.addChild(julien, herve)
-  x.addRelationship(julien, 'child', agnesMeunier)
+  creator.addChild(clement, herve)
+  creator.addRelationship(clement, 'child', agnesMeunier)
+  creator.addChild(julien, herve)
+  creator.addRelationship(julien, 'child', agnesMeunier)
 
-  x.addChild(chloe, xavier)
-  x.addRelationship(chloe, 'child', corinne)
-  x.addChild(annah, xavier)
-  x.addRelationship(annah, 'child', corinne)
+  creator.addChild(chloe, xavier)
+  creator.addRelationship(chloe, 'child', corinne)
+  creator.addChild(annah, xavier)
+  creator.addRelationship(annah, 'child', corinne)
 
-  x.addChild(eloise, philippe)
-  x.addRelationship(eloise, 'child', elodie)
+  creator.addChild(eloise, romain)
+  creator.addRelationship(eloise, 'child', elodie)
 
-  return x.familyTree
+  return creator.familyTree
 }
 //#endregion
 
@@ -101,18 +101,106 @@ describe('Family-tree-inspector', () => {
   let sut: FamilyTreeInspector
 
   beforeEach(() => {
-    sut = new FamilyTreeInspector(createBertholletTree())
+    createBertholletTree()
+    sut = new FamilyTreeInspector()
   })
 
   test('it creates the inspector', () => {
     expect(sut).toBeInstanceOf(FamilyTreeInspector)
   })
 
-  test('it finds siblings', () => {
-    //! error : inversed family relationships
+  test('it finds 0 siblings', () => {
+    const actual = sut.findPeopleByRelationship(pierrot, 'sibling')
+    expect(actual.length).toBe(0)
+  })
 
-    const actual = sut.findPeopleByRelationships(arnaud, 'sibling')
+  test('it finds 1 sibling', () => {
+    const actual = sut.findPeopleByRelationship(arnaud, 'sibling')
     expect(actual.length).toBe(1)
     expect(actual[0]).toBe(thomas)
+  })
+
+  test('it finds 2 siblings', () => {
+    const cat = new Person('Mojito', 'Cat', 'male')
+    const rel: Relation = { person: cat, relationship: 'parent' }
+    agnes.relations.push(rel)
+
+    const actual = sut.findSiblings(arnaud)
+
+    expect(actual.length).toBe(2)
+    expect(actual[0]).toBe(thomas)
+    expect(actual[1]).toBe(cat)
+  })
+
+  test('it finds 0 uncle-aunt', () => {
+    const actual = sut.findPeopleByRelationship(loic, 'uncle-aunt')
+    expect(actual.length).toBe(0)
+  })
+
+  test('it finds uncle(s) & aunt(s)', () => {
+    const actual = sut.findPeopleByRelationship(arnaud, 'uncle-aunt')
+
+    expect(actual.length).toBe(3)
+    expect(actual).toContain(herve)
+    expect(actual).toContain(xavier)
+    expect(actual).toContain(philippe)
+  })
+
+  test('it finds 0 nephew-niece', () => {
+    const actual = sut.findPeopleByRelationship(arnaud, 'nephew-niece')
+    expect(actual.length).toBe(0)
+  })
+
+  test('it finds 2 nephews-nieces', () => {
+    const actual = sut.findPeopleByRelationship(loic, 'nephew-niece')
+    expect(actual.length).toBe(2)
+    expect(actual).toContain(romain)
+    expect(actual).toContain(anthony)
+  })
+
+  test('it finds 0 cousin', () => {
+    const actual = sut.findPeopleByRelationship(loic, 'cousin')
+    expect(actual.length).toBe(0)
+  })
+
+  test('it finds 6 cousins', () => {
+    const actual = sut.findPeopleByRelationship(arnaud, 'cousin')
+    expect(actual.length).toBe(6)
+    expect(actual).toContain(romain)
+    expect(actual).toContain(anthony)
+    expect(actual).toContain(clement)
+    expect(actual).toContain(julien)
+    expect(actual).toContain(chloe)
+    expect(actual).toContain(annah)
+  })
+
+  test('it finds 6 cousins', () => {
+    const actual = sut.findPeopleByRelationship(chloe, 'cousin')
+    expect(actual.length).toBe(4)
+    expect(actual).toContain(clement)
+    expect(actual).toContain(julien)
+    expect(actual).toContain(arnaud)
+    expect(actual).toContain(thomas)
+  })
+
+  test('it finds 0 grand-parent', () => {
+    const actual = sut.findPeopleByRelationship(loic, 'grandParent')
+    expect(actual.length).toBe(0)
+  })
+
+  test('it finds 4 grand-parents', () => {
+    const actual = sut.findPeopleByRelationship(arnaud, 'grandParent')
+    expect(actual.length).toBe(4)
+    expect(actual).toContain(annick)
+    expect(actual).toContain(jo)
+    expect(actual).toContain(pierrot)
+    expect(actual).toContain(millette)
+  })
+
+  test('it finds 2 grand-parents', () => {
+    const actual = sut.findPeopleByRelationship(chloe, 'grandParent')
+    expect(actual.length).toBe(2)
+    expect(actual).toContain(pierrot)
+    expect(actual).toContain(millette)
   })
 })
--- src/pages/ts/exos/objects-relations/family-tree-inspector.ts
+++ src/pages/ts/exos/objects-relations/family-tree-inspector.ts
@@ -6,64 +6,114 @@ export type FamilyRelationShip = Relationship | 'sibling' | 'uncle-aunt' | 'neph
  * allow to inspect family (find brother...)
  */
 export class FamilyTreeInspector {
-  constructor(private familyTree: FamilyTree) {}
-
-  /**
-   * find all relations for a given person
-   * @param person the person whose family relationships we are looking for
-   * @param relations the searched relation(s)
-   * @returns an array of persons
-   */
-  findPeopleByRelationships(person: Person, relations: FamilyRelationShip | FamilyRelationShip[]): Person[] {
-    if (Array.isArray(relations)) {
-      // relations is an array of relationships
-      const res: Person[] = []
-      for (const relation of relations) {
-        const people = this.findPeopleByRelationship(person, relation)
-        res.push(...people)
-      }
-      return res
-    } else {
-      // relations is a single relationship
-      return this.findPeopleByRelationship(person, relations)
-    }
-  }
-
   /**
    * find all relations for a given person
    * @param person the person whose family relationships we are looking for
    * @param relation the searched relation
    * @returns an array of persons
    */
-  private findPeopleByRelationship(person: Person, relation: FamilyRelationShip): Person[] {
+  findPeopleByRelationship(person: Person, relation: FamilyRelationShip): Person[] {
     switch (relation) {
       case 'sibling':
         return this.findSiblings(person)
 
+      case 'uncle-aunt':
+        return this.findUnclesAunts(person)
+
+      case 'nephew-niece':
+        return this.findNephewsNieces(person)
+
+      case 'cousin':
+        return this.findCousins(person)
+
+      case 'grandParent':
+        return this.findGrandParents(person)
+
       default:
-        break
+        throw new Error(`Not implemented: ${relation}`)
     }
-    return []
   }
 
-  findSiblings(sibling: Person): Person[] {
-    // ajouter les enfants au resultat
-    //    sauf si enfant = person
-    //    sauf si enfant déja ajouté au résultat
+  findGrandParents(person: Person): Person[] {
+    const res: Person[] = []
+    const parents = this.findPeople(person, 'child')
+    for (const parent of parents) {
+      const grandParents = this.findPeople(parent, 'child')
+      res.push(...grandParents)
+    }
+    return res
+  }
 
-    // ----- trouver les parents
-    const parents = this.findPeople(sibling, 'parent')
+  findCousins(person: Person): Person[] {
+    const res: Person[] = []
+    // find uncles-aunts
+    const uncles = this.findUnclesAunts(person)
+    console.log(uncles.map((p) => p.firstname))
 
-    // ----- pour chaque parent, trouver les enfants
+    // for each uncles-aunts, find children
+    for (const uncle of uncles) {
+      const children = this.findPeople(uncle, 'parent')
+      res.push(...children)
+    }
+
+    // add found children to result
+    return res
+  }
+
+  findNephewsNieces(person: Person): Person[] {
+    const res: Person[] = []
+    // find siblings
+    const siblings = this.findSiblings(person)
+
+    // for each sibling, find children
+    for (const sibling of siblings) {
+      const children = this.findPeople(sibling, 'parent')
+      // add children to result
+      res.push(...children)
+    }
+
+    return res
+  }
+
+  findUnclesAunts(person: Person): Person[] {
+    const res: Person[] = []
+    // ----- find parents
+    const parents = this.findPeople(person, 'child')
+    // ----- for each parent, find siblings
+    for (const parent of parents) {
+      const siblings = this.findSiblings(parent)
+      res.push(...siblings)
+    }
+    return res
+  }
+
+  findSiblings(person: Person): Person[] {
+    // ----- find parents
+    const parents = this.findPeople(person, 'child')
+
+    // ----- for each parent, find children
     const children: Person[] = []
     for (const parent of parents) {
-      const kids = this.findPeople(parent, 'child')
-      console.log('FamilyTreeInspector ~ findSiblings ~ kids:', parent, kids)
+      const kids = this.findPeople(parent, 'parent')
+
+        // ----- filter to remove the sibling and the already found sibling
+        .filter((kid) => {
+          // ----- remove given person
+          if (kid === person) {
+            return false
+          }
+
+          // ----- remove already found sibling
+          return !children.some((child) => child === kid)
+        })
+
+      // ----- add found kids to result (=children)
+      children.push(...kids)
     }
-    return []
+    return children
   }
 
-  findPeople(person: Person, relationship: Relationship): Person[] {
+  private findPeople(person: Person, relationship: Relationship): Person[] {
     return person.relations
       .filter((relation) => relation.relationship === relationship)
       .map((relation) => relation.person)
