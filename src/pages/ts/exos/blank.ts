31f1944fb13c3e210180de52163d43611513cc2b
Arnaud Berthollet <arnaudberthollet@hotmail.fr>
2024-09-01 18:05:47 +0200

    add full TS/HTML/CSS playground

--- src/pages/ts/exos/blank.ts
+++
@@ -1,7 +0,0 @@
-;(() => {
-  const textAreas = document.getElementsByTagName('textarea')
-  if (textAreas.length > 0) {
-    const textArea = textAreas[0]
-    textArea.value = 'This file is for testing some code'
-  }
-})()
